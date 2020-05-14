3cc1b8c4ac9e6878ac351f53322eb7e29f391a5f
amitrehalia <amit.reh@gmail.com>
2020-05-14 17:53:36 +0530

    Java Program file

---
+++ Program.java
@@ -0,0 +1,13 @@
+import java.util.Date;
+
+public class Program {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println(" First Java Program");
+		Date dd = new Date();
+		System.out.println("Current Date is "+dd);
+
+	}
+
+}
